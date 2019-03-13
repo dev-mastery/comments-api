71d4aa880837439b81e48a1e0dd2c47fdf226a19
Bill Sourour <bill.sourour@arcnovus.com>
2019-03-12 22:47:29 -0400

    implement get comments

--- src/comment/service/get-comments.js
+++
@@ -1,8 +0,0 @@
-import makeComment from '../comment'
-export default function makeGetComments ({ commentsDb }) {
-  return async function getComments ({ postId } = {}) {
-    if (!postId) {
-      throw new Error('You must supply a post id.')
-    }
-  }
-}
