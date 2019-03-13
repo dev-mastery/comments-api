fd273d7dce3cebad41ac0664998973b8137d9026
Bill Sourour <bill.sourour@arcnovus.com>
2019-03-12 22:47:18 -0400

    fix test

--- src/comment/controller/put-comment.spec.js
+++ src/comment/controller/put-comment.spec.js
@@ -1,30 +1,38 @@
-import makePostComment from './post-comment'
+import makePutComment from './put-comment'
 import makeFakeComment from '../../../__test__/fixtures/comment'
 
-describe('post comment controller', () => {
-  it('successfully posts a comment', async () => {
-    const postComment = makePostComment({ addComment: c => c })
+describe('put comment controller', () => {
+  it('successfully puts a comment', async () => {
+    const fakeComment = makeFakeComment()
+    const putComment = makePutComment({ editComment: c => c })
     const request = {
-      body: makeFakeComment()
+      params: {
+        id: fakeComment.id
+      },
+      body: fakeComment
     }
     const expected = {
       headers: {
         'Content-Type': 'application/json'
       },
-      statusCode: 201,
+      statusCode: 200,
       body: request.body
     }
-    const actual = await postComment(request)
+    const actual = await putComment(request)
     expect(actual).toEqual(expected)
   })
   it('reports user errors', async () => {
-    const postComment = makePostComment({
-      addComment: () => {
+    const fakeComment = makeFakeComment()
+    const putComment = makePutComment({
+      editComment: () => {
         throw Error('Pow!')
       }
     })
     const request = {
-      body: makeFakeComment()
+      params: {
+        id: fakeComment.id
+      },
+      body: fakeComment
     }
     const expected = {
       headers: {
@@ -33,7 +41,7 @@ describe('post comment controller', () => {
       statusCode: 400,
       body: { error: 'Pow!' }
     }
-    const actual = await postComment(request)
+    const actual = await putComment(request)
     expect(actual).toEqual(expected)
   })
 })
