bce63ad1f0127c5c966c0df45933ab08cd56d9ea
Bill Sourour <bill.sourour@arcnovus.com>
2019-03-17 10:03:54 -0400

    whitelist code shares

---
+++ src/utils/sanitize.js
@@ -0,0 +1,7 @@
+import sanitizeHtml from 'sanitize-html'
+export default function sanitize (text) {
+  // TODO: allow more coding embeds
+  return sanitizeHtml(text, {
+    allowedIframeHostnames: ['codesandbox.io', 'repl.it']
+  })
+}
